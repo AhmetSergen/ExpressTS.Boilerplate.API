c52c82a20c159c5b690f0854f1bdab6584b6701f
Ahmet Sergen Boncuk <ahmet.sergen7@gmail.com>
2024-11-24 19:13:56 +0300

    removed old compiled js files

--- src/data/users.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.users = void 0;
-exports.users = [
-    { id: 1, name: "John Doe", email: "john.doe@example.com" },
-    { id: 2, name: "Jane Smith", email: "jane.smith@example.com" },
-];
